19692c345b6f11c2a815a417df99ac1a5b87e25d
marlonsg6 <marlonguedes4@gmail.com>
2024-07-26 23:53:20 -0300

    button and links updates2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const hamburgerButton = document.querySelector("#hamburgerButton");
+const closeButton = document.querySelector("#closeButton");
+const mobileMenu = document.querySelector("#mobileMenu");
+const imagens = document.querySelectorAll('.galeria li img');
+const cartItemsContainer = document.querySelector('#cart-items')
+const cartTotal = document.querySelector('#cart-total');
+
+
 //menu hamburger
-const hamburgerButton = document.querySelector("#hamburgerButton")
-const closeButton = document.querySelector("#closeButton")
-
-const mobileMenu = document.querySelector("#mobileMenu")
-
 hamburgerButton.addEventListener("click", function () {
     mobileMenu.classList.add("flex");
 })
@@ -12,9 +15,8 @@ closeButton.addEventListener("click", function () {
     mobileMenu.classList.remove("flex");
 })
 
-//galeria de img
-const imagens = document.querySelectorAll('.galeria li img');
 
+//galeria de img
 function galeriaTrocar(galeria) {
     const clicada = galeria.currentTarget;
     const principal = document.querySelector('.principal');
@@ -29,6 +31,7 @@ function galeriaClique(imagem) {
 imagens.forEach(galeriaClique);
 
 //Contador carrinho
+let cart = [];
 
 
 
@@ -38,6 +41,36 @@ imagens.forEach(galeriaClique);
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Função para preencher os campos com os valores do localStorage
+    preencherCampos();
+
+    // Evento para o botão "CONFIRMAR PAGAMENTO"
+    document.getElementById('confirmButton').addEventListener('click', function () {
+        alert('Botão Confirmar Pagamento clicado'); // Alerta para verificação
+        window.location.href = './pedido-concluido.html';
+    });
+
+    // Evento para o botão "VOLTAR"
+    document.getElementById('backButton2').addEventListener('click', function () {
+        window.location.href = './index.html';
+    });
+});
+
+
+
+// Função para preencher os campos com os valores do localStorage
+function preencherCampos() {
+    document.getElementById('personalData').innerText = 'Destinatário: ' + localStorage.getItem('destinatario') +
+        ' | Contato: ' + localStorage.getItem('contato') +
+        ' | CPF: ' + localStorage.getItem('cpf') +
+        ' | Email: ' + localStorage.getItem('email');
+
+    document.getElementById('addressData').innerText = 'Endereço: ' + localStorage.getItem('logradouro') +
+        ', ' + localStorage.getItem('complemento') +
+        ' - ' + localStorage.getItem('cep') +
+        '. Ponto de Referência: ' + localStorage.getItem('pontoReferencia');
+}
 
 // Função para salvar os dados do formulário no localStorage
 function salvarDadosLocalStorage() {
